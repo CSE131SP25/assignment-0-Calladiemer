262ed71c3654b4751bafc3b1d3b4cea8399a50c8
calladiemer <calladiemer@localhost>
2025-01-21 15:13:21 -0600

    Assignment 0 done - Robot

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -31,6 +31,8 @@ public class RobotInstructions  {
 		 */
 		robot.forward(60);
 		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
 		robot.forward(80);
